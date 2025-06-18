3e7528c75b6b29149fb59dae7e9ba14268a24896
Usuario <miguel97munoz@gmail.com>
2025-06-18 21:23:10 +0200

    18/06/2025

--- test.py
+++ test.py
@@ -1,2 +1,18 @@
 nombre = "miguel"
-edad = 28
+print(nombre)
+
+n1 = 10
+n2 = 40
+print(n1+n2)
+
+n1 = 10
+n2 = 40
+print(n1//n2) #devuelve el resultado sin decimales
+
+n1 = 10
+n2 = 40
+print(n1%n2) #devuelve el modulo (para ver si es par o impar)
+
+n1 = int(input("Dame un numero "))
+n2 = int(input("Dame otro numero "))
+print(n1+n2)
