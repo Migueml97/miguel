c3cc1cbb8cc883825079d9bcc036692448ce64c0
Usuario <miguel97munoz@gmail.com>
2025-06-22 14:58:37 +0200

    ascensor

--- miguel.py
+++ miguel.py
@@ -1,3 +1,4 @@
+#Ejercicio 1 -> Ascensor
 piso = int(input("Estás en un ascensor en la planta cero y tiene las siguentes plantas: " \
 " 0 -> Hall" \
 " 1 -> Comedor" \
@@ -28,44 +29,6 @@ while piso != 6:
     piso = int(input("Introduce un número del 1 al 6: "))
 print("Adios")
 
-
-
-
-
-
-#for piso in range(0,7):
-#   if piso != 6:
-#      if piso >= 0 and piso <=5:
-#            piso_actual = 0
-#            if piso > piso_actual:
-#                print("Subiendo..")
-#                for planta in range(piso_actual, piso + 1):
-#                    print(f"Ascensor en la planta {planta}")   
-#            elif piso < piso_actual:
-#                print("Bajando...")
-#                for planta in range(piso_actual, piso - 1):
-#                    print(f"Ascensor en la planta {planta}")
-#            else:
-#                print("Ya estas en la planta solicitada")
-#            print(f"Has llegado a la planta {piso}")
-#            piso = int(input("Introduce un número del 1 al 6: "))
-#        else:
-#            piso = int(input("Introduce un número del 1 al 6: "))
-#else:
-#    print("Adios")
-
-
-
-#nuevo_piso = int(input("¿A qué planta quieres ir ahora?"))
-        #if nuevo_piso == 6:
-        #    if nuevo_piso >= 0 and nuevo_piso >= 5:
-        #        piso_actual = piso
-        #        if nuevo_piso > piso_actual:
-        #            print("subiendo...")
-        #            for planta in range(piso_actual, nuevo_piso + 1):
-        #                print(f"Ascensor en la planta {planta}")
-        #        elif nuevo_piso < piso_actual:
-        #            print("Bajando...")
 
             
 
